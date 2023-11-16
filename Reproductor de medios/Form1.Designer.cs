a3803cef394949e00fc1fc3164fa515fad543632
ehalfonso <ehalfonso@uts.edu.mx>
2023-11-16 15:01:21 -0700

    Agregar archivos de proyecto.

---
+++ Reproductor de medios/Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace Reproductor_de_medios
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			panelSideMenu = new Panel();
+			panelMedioSubMenu = new Panel();
+			button5 = new Button();
+			button4 = new Button();
+			button3 = new Button();
+			button2 = new Button();
+			btnMedia = new Button();
+			panel1 = new Panel();
+			button1 = new Button();
+			panel2 = new Panel();
+			button6 = new Button();
+			button7 = new Button();
+			button8 = new Button();
+			button9 = new Button();
+			button10 = new Button();
+			panel3 = new Panel();
+			button11 = new Button();
+			button12 = new Button();
+			button13 = new Button();
+			button14 = new Button();
+			panelSideMenu.SuspendLayout();
+			panelMedioSubMenu.SuspendLayout();
+			panel2.SuspendLayout();
+			panel3.SuspendLayout();
+			SuspendLayout();
+			// 
+			// panelSideMenu
+			// 
+			panelSideMenu.BackColor = Color.FromArgb(71, 76, 236);
+			panelSideMenu.Controls.Add(panel3);
+			panelSideMenu.Controls.Add(button10);
+			panelSideMenu.Controls.Add(panel2);
+			panelSideMenu.Controls.Add(button1);
+			panelSideMenu.Controls.Add(panelMedioSubMenu);
+			panelSideMenu.Controls.Add(btnMedia);
+			panelSideMenu.Controls.Add(panel1);
+			panelSideMenu.Dock = DockStyle.Left;
+			panelSideMenu.Location = new Point(0, 0);
+			panelSideMenu.Name = "panelSideMenu";
+			panelSideMenu.Size = new Size(250, 789);
+			panelSideMenu.TabIndex = 0;
+			// 
+			// panelMedioSubMenu
+			// 
+			panelMedioSubMenu.BackColor = Color.FromArgb(53, 57, 178);
+			panelMedioSubMenu.Controls.Add(button5);
+			panelMedioSubMenu.Controls.Add(button4);
+			panelMedioSubMenu.Controls.Add(button3);
+			panelMedioSubMenu.Controls.Add(button2);
+			panelMedioSubMenu.Dock = DockStyle.Top;
+			panelMedioSubMenu.Location = new Point(0, 145);
+			panelMedioSubMenu.Name = "panelMedioSubMenu";
+			panelMedioSubMenu.Size = new Size(250, 163);
+			panelMedioSubMenu.TabIndex = 1;
+			// 
+			// button5
+			// 
+			button5.Dock = DockStyle.Top;
+			button5.FlatAppearance.BorderSize = 0;
+			button5.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64);
+			button5.FlatStyle = FlatStyle.Flat;
+			button5.ForeColor = SystemColors.ButtonHighlight;
+			button5.Location = new Point(0, 120);
+			button5.Name = "button5";
+			button5.Padding = new Padding(35, 0, 0, 0);
+			button5.Size = new Size(250, 40);
+			button5.TabIndex = 4;
+			button5.Text = "button5";
+			button5.TextAlign = ContentAlignment.MiddleLeft;
+			button5.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			button4.Dock = DockStyle.Top;
+			button4.FlatAppearance.BorderSize = 0;
+			button4.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64);
+			button4.FlatStyle = FlatStyle.Flat;
+			button4.ForeColor = SystemColors.ButtonHighlight;
+			button4.Location = new Point(0, 80);
+			button4.Name = "button4";
+			button4.Padding = new Padding(35, 0, 0, 0);
+			button4.Size = new Size(250, 40);
+			button4.TabIndex = 3;
+			button4.Text = "button4";
+			button4.TextAlign = ContentAlignment.MiddleLeft;
+			button4.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			button3.Dock = DockStyle.Top;
+			button3.FlatAppearance.BorderSize = 0;
+			button3.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64);
+			button3.FlatStyle = FlatStyle.Flat;
+			button3.ForeColor = SystemColors.ButtonHighlight;
+			button3.Location = new Point(0, 40);
+			button3.Name = "button3";
+			button3.Padding = new Padding(35, 0, 0, 0);
+			button3.Size = new Size(250, 40);
+			button3.TabIndex = 2;
+			button3.Text = "button3";
+			button3.TextAlign = ContentAlignment.MiddleLeft;
+			button3.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			button2.Dock = DockStyle.Top;
+			button2.FlatAppearance.BorderSize = 0;
+			button2.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64);
+			button2.FlatStyle = FlatStyle.Flat;
+			button2.ForeColor = SystemColors.ButtonHighlight;
+			button2.Location = new Point(0, 0);
+			button2.Name = "button2";
+			button2.Padding = new Padding(35, 0, 0, 0);
+			button2.Size = new Size(250, 40);
+			button2.TabIndex = 1;
+			button2.Text = "button2";
+			button2.TextAlign = ContentAlignment.MiddleLeft;
+			button2.UseVisualStyleBackColor = true;
+			// 
+			// btnMedia
+			// 
+			btnMedia.Dock = DockStyle.Top;
+			btnMedia.FlatAppearance.BorderSize = 0;
+			btnMedia.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64);
+			btnMedia.FlatStyle = FlatStyle.Flat;
+			btnMedia.ForeColor = SystemColors.ButtonHighlight;
+			btnMedia.Location = new Point(0, 100);
+			btnMedia.Name = "btnMedia";
+			btnMedia.Padding = new Padding(10, 0, 0, 0);
+			btnMedia.Size = new Size(250, 45);
+			btnMedia.TabIndex = 1;
+			btnMedia.Text = "Media";
+			btnMedia.TextAlign = ContentAlignment.MiddleLeft;
+			btnMedia.UseVisualStyleBackColor = true;
+			// 
+			// panel1
+			// 
+			panel1.Dock = DockStyle.Top;
+			panel1.Location = new Point(0, 0);
+			panel1.Name = "panel1";
+			panel1.Size = new Size(250, 100);
+			panel1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			button1.Dock = DockStyle.Top;
+			button1.FlatAppearance.BorderSize = 0;
+			button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64);
+			button1.FlatStyle = FlatStyle.Flat;
+			button1.ForeColor = SystemColors.ButtonHighlight;
+			button1.Location = new Point(0, 308);
+			button1.Name = "button1";
+			button1.Padding = new Padding(10, 0, 0, 0);
+			button1.Size = new Size(250, 45);
+			button1.TabIndex = 2;
+			button1.Text = "Play list";
+			button1.TextAlign = ContentAlignment.MiddleLeft;
+			button1.UseVisualStyleBackColor = true;
+			// 
+			// panel2
+			// 
+			panel2.BackColor = Color.FromArgb(53, 57, 178);
+			panel2.Controls.Add(button6);
+			panel2.Controls.Add(button7);
+			panel2.Controls.Add(button8);
+			panel2.Controls.Add(button9);
+			panel2.Dock = DockStyle.Top;
+			panel2.Location = new Point(0, 353);
+			panel2.Name = "panel2";
+			panel2.Size = new Size(250, 163);
+			panel2.TabIndex = 3;
+			// 
+			// button6
+			// 
+			button6.Dock = DockStyle.Top;
+			button6.FlatAppearance.BorderSize = 0;
+			button6.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64);
+			button6.FlatStyle = FlatStyle.Flat;
+			button6.ForeColor = SystemColors.ButtonHighlight;
+			button6.Location = new Point(0, 120);
+			button6.Name = "button6";
+			button6.Padding = new Padding(35, 0, 0, 0);
+			button6.Size = new Size(250, 40);
+			button6.TabIndex = 4;
+			button6.Text = "button6";
+			button6.TextAlign = ContentAlignment.MiddleLeft;
+			button6.UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			button7.Dock = DockStyle.Top;
+			button7.FlatAppearance.BorderSize = 0;
+			button7.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64);
+			button7.FlatStyle = FlatStyle.Flat;
+			button7.ForeColor = SystemColors.ButtonHighlight;
+			button7.Location = new Point(0, 80);
+			button7.Name = "button7";
+			button7.Padding = new Padding(35, 0, 0, 0);
+			button7.Size = new Size(250, 40);
+			button7.TabIndex = 3;
+			button7.Text = "button7";
+			button7.TextAlign = ContentAlignment.MiddleLeft;
+			button7.UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			button8.Dock = DockStyle.Top;
+			button8.FlatAppearance.BorderSize = 0;
+			button8.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64);
+			button8.FlatStyle = FlatStyle.Flat;
+			button8.ForeColor = SystemColors.ButtonHighlight;
+			button8.Location = new Point(0, 40);
+			button8.Name = "button8";
+			button8.Padding = new Padding(35, 0, 0, 0);
+			button8.Size = new Size(250, 40);
+			button8.TabIndex = 2;
+			button8.Text = "button8";
+			button8.TextAlign = ContentAlignment.MiddleLeft;
+			button8.UseVisualStyleBackColor = true;
+			// 
+			// button9
+			// 
+			button9.Dock = DockStyle.Top;
+			button9.FlatAppearance.BorderSize = 0;
+			button9.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64);
+			button9.FlatStyle = FlatStyle.Flat;
+			button9.ForeColor = SystemColors.ButtonHighlight;
+			button9.Location = new Point(0, 0);
+			button9.Name = "button9";
+			button9.Padding = new Padding(35, 0, 0, 0);
+			button9.Size = new Size(250, 40);
+			button9.TabIndex = 1;
+			button9.Text = "button9";
+			button9.TextAlign = ContentAlignment.MiddleLeft;
+			button9.UseVisualStyleBackColor = true;
+			// 
+			// button10
+			// 
+			button10.Dock = DockStyle.Top;
+			button10.FlatAppearance.BorderSize = 0;
+			button10.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64);
+			button10.FlatStyle = FlatStyle.Flat;
+			button10.ForeColor = SystemColors.ButtonHighlight;
+			button10.Location = new Point(0, 516);
+			button10.Name = "button10";
+			button10.Padding = new Padding(10, 0, 0, 0);
+			button10.Size = new Size(250, 45);
+			button10.TabIndex = 4;
+			button10.Text = "Ecualizador";
+			button10.TextAlign = ContentAlignment.MiddleLeft;
+			button10.UseVisualStyleBackColor = true;
+			// 
+			// panel3
+			// 
+			panel3.BackColor = Color.FromArgb(53, 57, 178);
+			panel3.Controls.Add(button11);
+			panel3.Controls.Add(button12);
+			panel3.Controls.Add(button13);
+			panel3.Controls.Add(button14);
+			panel3.Dock = DockStyle.Top;
+			panel3.Location = new Point(0, 561);
+			panel3.Name = "panel3";
+			panel3.Size = new Size(250, 163);
+			panel3.TabIndex = 5;
+			// 
+			// button11
+			// 
+			button11.Dock = DockStyle.Top;
+			button11.FlatAppearance.BorderSize = 0;
+			button11.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64);
+			button11.FlatStyle = FlatStyle.Flat;
+			button11.ForeColor = SystemColors.ButtonHighlight;
+			button11.Location = new Point(0, 120);
+			button11.Name = "button11";
+			button11.Padding = new Padding(35, 0, 0, 0);
+			button11.Size = new Size(250, 40);
+			button11.TabIndex = 4;
+			button11.Text = "button11";
+			button11.TextAlign = ContentAlignment.MiddleLeft;
+			button11.UseVisualStyleBackColor = true;
+			// 
+			// button12
+			// 
+			button12.Dock = DockStyle.Top;
+			button12.FlatAppearance.BorderSize = 0;
+			button12.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64);
+			button12.FlatStyle = FlatStyle.Flat;
+			button12.ForeColor = SystemColors.ButtonHighlight;
+			button12.Location = new Point(0, 80);
+			button12.Name = "button12";
+			button12.Padding = new Padding(35, 0, 0, 0);
+			button12.Size = new Size(250, 40);
+			button12.TabIndex = 3;
+			button12.Text = "button12";
+			button12.TextAlign = ContentAlignment.MiddleLeft;
+			button12.UseVisualStyleBackColor = true;
+			// 
+			// button13
+			// 
+			button13.Dock = DockStyle.Top;
+			button13.FlatAppearance.BorderSize = 0;
+			button13.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64);
+			button13.FlatStyle = FlatStyle.Flat;
+			button13.ForeColor = SystemColors.ButtonHighlight;
+			button13.Location = new Point(0, 40);
+			button13.Name = "button13";
+			button13.Padding = new Padding(35, 0, 0, 0);
+			button13.Size = new Size(250, 40);
+			button13.TabIndex = 2;
+			button13.Text = "button13";
+			button13.TextAlign = ContentAlignment.MiddleLeft;
+			button13.UseVisualStyleBackColor = true;
+			// 
+			// button14
+			// 
+			button14.Dock = DockStyle.Top;
+			button14.FlatAppearance.BorderSize = 0;
+			button14.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64);
+			button14.FlatStyle = FlatStyle.Flat;
+			button14.ForeColor = SystemColors.ButtonHighlight;
+			button14.Location = new Point(0, 0);
+			button14.Name = "button14";
+			button14.Padding = new Padding(35, 0, 0, 0);
+			button14.Size = new Size(250, 40);
+			button14.TabIndex = 1;
+			button14.Text = "button14";
+			button14.TextAlign = ContentAlignment.MiddleLeft;
+			button14.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(951, 789);
+			Controls.Add(panelSideMenu);
+			Name = "Form1";
+			Text = "Form1";
+			panelSideMenu.ResumeLayout(false);
+			panelMedioSubMenu.ResumeLayout(false);
+			panel2.ResumeLayout(false);
+			panel3.ResumeLayout(false);
+			ResumeLayout(false);
+		}
+
+		#endregion
+
+		private Panel panelSideMenu;
+		private Panel panelMedioSubMenu;
+		private Button button5;
+		private Button button4;
+		private Button button3;
+		private Button button2;
+		private Button btnMedia;
+		private Panel panel1;
+		private Panel panel3;
+		private Button button11;
+		private Button button12;
+		private Button button13;
+		private Button button14;
+		private Button button10;
+		private Panel panel2;
+		private Button button6;
+		private Button button7;
+		private Button button8;
+		private Button button9;
+		private Button button1;
+	}
+}
---
+++ Reproductor de medios/Form1.cs
@@ -0,0 +1,10 @@
+namespace Reproductor_de_medios
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+	}
+}
